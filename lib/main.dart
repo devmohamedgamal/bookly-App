ab7cee2a4aac6852385cc03d9c818de01c9e22cd
devmohamedgamal <mpedo2010@gmail.com>
2023-09-10 15:10:37 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home:SplashView(),
+    );
+  }
+}
