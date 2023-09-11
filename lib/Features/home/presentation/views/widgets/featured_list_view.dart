887d403ac2f3f6d5c20c552a8070fa0238a13d67
devmohamedgamal <mpedo2010@gmail.com>
2023-09-11 14:50:28 +0300

    add style file and add text to home

---
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -0,0 +1,26 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'custom_list_view_item.dart';
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200.h,
+      child: ListView.builder(
+          itemCount: 20,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8) ,
+              child: CustomListViewItem(),
+            );
+          }),
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../../core/utils/styles.dart';
 import 'custom_app_bar.dart';
-import 'custom_list_view_item.dart';
+import 'featured_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -11,32 +12,15 @@ class HomeViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 60),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const CustomAppBar(),
           SizedBox(height: 30.h),
           const FeaturedBooksListView(),
+          SizedBox(height: 50.h),
+          const Text("Best Seller", style: Styles.titleMedium),
         ],
       ),
-    );
-  }
-}
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 200.h,
-      child: ListView.builder(
-          itemCount: 20,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8) ,
-              child: CustomListViewItem(),
-            );
-          }),
     );
   }
 }
