41881965447d14ff0da79e4a95a22b86f9d473fc
devmohamedgamal <mpedo2010@gmail.com>
2023-09-16 15:41:58 +0300

    add text to book details view

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/styles.dart';
 import 'custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -8,16 +9,36 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30),
+          padding: const EdgeInsets.symmetric(horizontal: 30),
           child: Column(
             children: [
-              CustomBookDetailsAppBar(),
-              CustomBookImage(
+              const CustomBookDetailsAppBar(),
+              const SizedBox(
+                height: 40,
+              ),
+              const CustomBookImage(
                 height: 225,
                 width: 150,
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const Text(
+                'Make Time',
+                style: Styles.textStyle30,
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              Text(
+                'Mohamed Gamal',
+                style: Styles.textStyle18.copyWith(
+                  fontStyle: FontStyle.italic,
+                  color: Colors.white.withOpacity(0.7),
+                ),
               ),
             ],
           ),
