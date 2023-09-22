4798667db3668b05e0902526b67fc170199c2bed
devmohamedgamal <mpedo2010@gmail.com>
2023-09-22 14:46:28 +0300

    cached network image

--- lib/Features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/core/utils/assets_manger.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -10,17 +11,28 @@ class CustomBookImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+      ),
       height: height.h,
       width: width.w,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16.r),
-        image: DecorationImage(
-          image: NetworkImage(
-            imageUrl ?? "https://www.shoroukbookstores.com/images/Books/original/9781847941831.jpg",
-          ),
-          fit: BoxFit.fill,
+      child: CachedNetworkImage(
+        fit: BoxFit.fill,
+        placeholder: (context, url) => const Center(child: CircularProgressIndicator(color: Colors.red,),),
+        imageUrl: imageUrl ?? "https://www.shoroukbookstores.com/images/Books/original/9781847941831.jpg",
+        errorWidget: (context, url, error) => const Icon(Icons.error),
         ),
-      ),
     );
   }
 }
+
+
+// decoration: BoxDecoration(
+//         borderRadius: BorderRadius.circular(16.r),
+//         image: DecorationImage(
+//           image: NetworkImage(
+//             imageUrl ?? "https://www.shoroukbookstores.com/images/Books/original/9781847941831.jpg",
+//           ),
+//           fit: BoxFit.fill,
+//         ),
+//       ),
